01f3ede5418417d84dc6cc35276717b365a63e58
emilaynerodrigues <emilaynx@gmail.com>
2026-09-08 20:06:24 -0400

    primeiro commit do contador

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  iniciarPagina();
+});
+
+function iniciarPagina() {
+  // Futuramente:
+  // - animação de entrada
+  // - estrelas
+  // - transição para o contador
+  // - contagem regressiva
+  // - animação especial no aniversário
+}
